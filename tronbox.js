f3a789e060a839ad808eb8fb46425a407b2b59b8
Gurkirat Singh <tablagurkirat@yahoo.com>
2018-12-07 04:14:54 -0800

    network config

--- tronbox.js
+++ tronbox.js
@@ -5,6 +5,16 @@ module.exports = {
       privateKey: 'da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0',
       consume_user_resource_percent: 30,
       fee_limit: 100000000,
+      fullNode: "https://127.0.0.1:8090",
+      solidityNode: "https://127.0.0.1:8091",
+      eventServer: "https://127.0.0.1:8092",
+      network_id: "*" // Match any network id
+    },
+    shasta: {
+      from: 'TPL66VK2gCXNCD7EJg9pgJRfqcRazjhUZY',
+      privateKey: 'da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0',
+      consume_user_resource_percent: 30,
+      fee_limit: 100000000,
       host: "https://api.trongrid.io",
       port: 8090,
       fullNode: "https://api.shasta.trongrid.io",
@@ -12,6 +22,14 @@ module.exports = {
       eventServer: "https://api.shasta.trongrid.io",
       network_id: "*" // Match any network id
     },
-    production: {}
+    mainnet: {
+      privateKey: process.env.PK,
+      consume_user_resource_percent: 30,
+      fee_limit: 100000000,
+      fullNode: "https://api.trongrid.io",
+      solidityNode: "https://api.trongrid.io",
+      eventServer: "https://api.trongrid.io",
+      network_id: "*"
+    }
   }
 };
